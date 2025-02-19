f825a6ddb1ea3b3966ede114f13b798e2378fbab
Hari-kato <hari.s@katomaran.com>
2025-02-19 19:07:24 +0530

    added new components

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import Story from "@/components/Story";
 import React from "react";
 
 const page = () => {
@@ -11,6 +14,9 @@ const page = () => {
       <HeroSection />
       <About />
       <Features />
+      <Story/>
+      <Contact/>
+      <Footer />
     </main>
   );
 };
